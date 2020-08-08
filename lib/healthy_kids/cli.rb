91d55928ad80807b1177a094d02d6e493b312b7b
Deborah Santillan <s.debb@yahoo.com>
2020-08-08 18:02:23 +0000

    test

--- lib/healthy_kids/cli.rb
+++ lib/healthy_kids/cli.rb
@@ -1,6 +1,16 @@
 class HealthyKids::CLI 
-  def call 
-    puts "Greetings!"
-    binding.pry 
+ def call 
+   puts "Greetings!"
+  # get_healthy_tip
+  # list_healthy_tip
+  
+  def get_healthy_tip
+    @tip = ["Prevent Illness", "Nutrition", "Recipes"]
   end 
-end 
+   
+   def list_healthy_tip
+     @tip.each_with_index(1) do |tip, index|
+       puts "#{index}. #{tip}"
+ end 
+end
+end  
