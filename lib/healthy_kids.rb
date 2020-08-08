125d6e9c4542d70ecefc1c4422023338ea588cd9
Deborah Santillan <s.debb@yahoo.com>
2020-08-08 15:00:34 +0000

    add description

---
+++ lib/healthy_kids.rb
@@ -0,0 +1,6 @@
+require "healthy_kids/version"
+
+module HealthyKids
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/healthy_kids/version.rb
@@ -0,0 +1,3 @@
+module HealthyKids
+  VERSION = "0.1.0"
+end
