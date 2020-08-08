5806524c38565023c7f6ec503eb174c460f9bb20
Deborah Santillan <s.debb@yahoo.com>
2020-08-08 19:44:00 +0000

    comment

--- lib/healthy_kids/cli.rb
+++ lib/healthy_kids/cli.rb
@@ -1,16 +1,15 @@
 class HealthyKids::CLI 
  def call 
    puts "Greetings!"
-  # get_healthy_tip
-  # list_healthy_tip
-  
-  def get_healthy_tip
-    @tip = ["Prevent Illness", "Nutrition", "Recipes"]
-  end 
-   
-   def list_healthy_tip
-     @tip.each.with_index(1) do |tip, index|
-       puts "#{index}. #{tip}"
  end 
-end
-end  
+ 
+def get_ad_month
+  @months = ['sep19', 'oct19', 'nov19']
+ end 
+ 
+ def user_month
+   @months.each_with_index  { |index, month|
+     puts "#{index} #{month}"
+   }
+ end 
+ end 
