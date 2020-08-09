172a7055f848b05c546a3fcb90baaba8ab0bab08
Deborah Santillan <s.debb@yahoo.com>
2020-08-09 19:22:17 +0000

    added lines

--- lib/healthy_kids.rb
+++ lib/healthy_kids.rb
@@ -1,5 +1,7 @@
 require_relative "healthy_kids/version"
 require_relative "healthy_kids/cli"
+# require_relative "healthy_kids/"
+
 
 require "pry"
 
--- lib/healthy_kids/cli.rb
+++ lib/healthy_kids/cli.rb
@@ -3,27 +3,35 @@ def call
   puts "Greetings!"
   labels
   selected_labels
+  get_labels
 end 
  
-def labels
-  puts "Make a selection<3"
+ def labels
   @labels = ['Nutritional', 'Recipes', 'Recommendations']
-  @labels.each_with_index  { |i, n|
-    puts "#{n + 1}. #{i}"
-  }
+end
+ 
+def selected_labels
+  puts "Make a selection<3"
+ @labels.each.with_index(1)  do |month, i|
+    puts "#{index}. #{month}"
  end 
  end
  
- def selected_labels
+ def get_labels
    selection = gets.strip
-   if binding.pry 
-    # if valid_input(selection, @labels)
-# end
- end 
- 
+  if valid_input(selection, @labels)
+    show_the_selection
+ end
+   
  def valid_input(input, data)
    input.to_i <= data.length && input.to_i > 0 
  end 
+ 
+ def show _selection_for(selection)
+   label = @labels[selection - 1]
+   puts "You selected: #{label}"
  end 
+ end 
+
  
  
