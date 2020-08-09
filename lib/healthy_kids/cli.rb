b12aaa7499e00ad1cf2ec3777739c0d976bfc99f
Deborah Santillan <s.debb@yahoo.com>
2020-08-09 00:14:40 +0000

    validation check

--- lib/healthy_kids/cli.rb
+++ lib/healthy_kids/cli.rb
@@ -1,14 +1,29 @@
 class HealthyKids::CLI 
 def call 
   puts "Greetings!"
-  labels 
+  labels
+  selected_labels
 end 
  
 def labels
-  @labels = ['Nutritional', 'Recipes', 'Recommended']
+  puts "Make a selection<3"
+  @labels = ['Nutritional', 'Recipes', 'Recommendations']
   @labels.each_with_index  { |i, n|
     puts "#{n + 1}. #{i}"
   }
  end 
  end
+ 
+ def selected_labels
+   selection = gets.strip
+   if binding.pry 
+    # if valid_input(selection, @labels)
+# end
+ end 
+ 
+ def valid_input(input, data)
+   input.to_i <= data.length && input.to_i > 0 
+ end 
+ end 
+ 
  
