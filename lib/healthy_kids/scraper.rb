63685d05ccf99a4dc3b8987d21f58a8d3f7a65e2
Deborah Santillan <s.debb@yahoo.com>
2020-08-09 22:01:41 +0000

    minor change scrape

--- lib/healthy_kids/scraper.rb
+++ lib/healthy_kids/scraper.rb
@@ -1,9 +1,7 @@
 class HealthyKids::Scraper 
-  def self.scrape_work
-    doc = Nokogiri::HTML(open(https://www.eatright.org/health/weight-loss/your-health-and-your-weight/helping-kids-maintain-a-healthy-body-weight-a-cheat-sheet-for-success:))
+  def scrape_work 
+   doc = Nokogiri::HTML(open("https://www.eatright.org/health/weight-loss/your-health-and-your-weight/helping-kids-maintain-a-healthy-body-weight-a-cheat-sheet-for-success"))
+   binding.pry 
+ end 
+ end 
     
-    months = doc.css
-    
-     months.each do |m|
-       name = m.text 
-       HealthyKids::Labeled.new(name)
