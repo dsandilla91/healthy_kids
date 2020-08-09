f227ebe0be90bddf8dce3a2dd97a10e174f11a54
Deborah Santillan <s.debb@yahoo.com>
2020-08-09 22:06:42 +0000

    added self to scrape

--- lib/healthy_kids/scraper.rb
+++ lib/healthy_kids/scraper.rb
@@ -1,5 +1,5 @@
 class HealthyKids::Scraper 
-  def scrape_work 
+  def self.scrape_work 
    doc = Nokogiri::HTML(open("https://www.eatright.org/health/weight-loss/your-health-and-your-weight/helping-kids-maintain-a-healthy-body-weight-a-cheat-sheet-for-success"))
    binding.pry 
  end 
