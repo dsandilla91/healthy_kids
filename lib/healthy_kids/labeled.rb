a37cff52089695a31c781aa1ea8fe38dfbdabb4c
Deborah Santillan <s.debb@yahoo.com>
2020-08-09 20:38:18 +0000

    update

---
+++ lib/healthy_kids/labeled.rb
@@ -0,0 +1,19 @@
+class HealthyKids::Labeled
+  @@all = []
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    save
+  end 
+  
+  def self.all 
+    HealthyKids::Scraper, scrape_months if @@all.empty
+    @@all
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+end 
+    
