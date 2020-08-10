04cc26c1cf88f7b2a45571e920615d47f6ac6581
Deborah Santillan <s.debb@yahoo.com>
2020-08-10 23:55:57 +0000

    updated

--- lib/healthy_kids/scraper.rb
+++ lib/healthy_kids/scraper.rb
@@ -2,7 +2,7 @@ class HealthyKids::Scraper
   def self.scrape_work 
    doc = Nokogiri::HTML(open("https://www.eatright.org/health/weight-loss/your-health-and-your-weight/helping-kids-maintain-a-healthy-body-weight-a-cheat-sheet-for-success"))
    
-   label = doc.css('ul.itemList li')
+   label = doc.css('div.grid_4.rail div.articleList.mod a')
    
    label.each do |m| 
    name = m.text
